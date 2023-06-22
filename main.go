3c54299a1d2442e97c0d5495a824be9b9750efa2
mohamed <mohamedabdelfatah2027@gmail.com>
2023-06-22 13:20:40 +0300

    wip

--- main.go
+++ main.go
@@ -8,34 +8,49 @@ import (
 	"strings"
 )
 
-func main() {
-	hook := os.Getenv("webhook")
-	event := os.Getenv("event")
-	author := os.Getenv("author")
-	commit := os.Getenv("commit")[0:6]
-	commit_url := os.Getenv("commit_url")
-	workflow_name := os.Getenv("workflow_name")
-	workflow_url := os.Getenv("workflow_url")
+type Context struct {
+	webhook       string
+	event         string
+	author        string
+	commit        string
+	commit_url    string
+	workflow_name string
+	workflow_url  string
+}
 
+func NewContext() *Context {
+	return &Context{
+		webhook:       os.Getenv("webhook"),
+		event:         os.Getenv("event"),
+		author:        os.Getenv("author"),
+		commit:        os.Getenv("commit")[0:6],
+		commit_url:    os.Getenv("commit_url"),
+		workflow_name: os.Getenv("workflow_name"),
+		workflow_url:  os.Getenv("workflow_url"),
+	}
+}
+
+func main() {
+	context := NewContext()
 	var mention string
-	if event == "pr" {
-		mention = getAuthorSlackID(author)
-	} else if event == "push" {
+	if context.event == "pr" {
+		mention = getAuthorSlackID(context.author)
+	} else if context.event == "push" {
 		mention = "!channel"
 	} else {
 		log.Fatal("event type should be specified")
 	}
 
 	message := fmt.Sprintf(`{"text": "
-	> *CI Failed*  
-	 *Commit* 
-	 <%s|%s>
-	 Workflow Failed: %s
-	 Workflow Url: %s
-	 <%s>"}`,
-		commit_url, commit, workflow_name, workflow_url, mention)
+	>*CI Failed*  
+	>*Commit* 
+	><%s|%s>
+	>Workflow Failed: %s
+	>Workflow Url: %s
+	><%s>"}`,
+		context.commit_url, context.commit, context.workflow_name, context.workflow_url, mention)
 	body := strings.NewReader(message)
-	_, err := http.Post(hook, "Content-type: application/json", body)
+	_, err := http.Post(context.webhook, "Content-type: application/json", body)
 	if err != nil {
 		log.Fatal(err)
 	}
