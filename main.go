61951ff354e746e47e2bfb560b008717efa0b230
mohamed <mohamedabdelfatah2027@gmail.com>
2023-06-22 12:31:00 +0300

    wip

--- main.go
+++ main.go
@@ -12,9 +12,8 @@ func main() {
 	hook := os.Getenv("webhook")
 	event := os.Getenv("event")
 	author := os.Getenv("author")
-	commit := os.Getenv("commit")[0:7]
+	commit := os.Getenv("commit")[0:6]
 	commit_url := os.Getenv("commit_url")
-	//run_url := os.Getenv("run_url")
 	workflow_name := os.Getenv("workflow_name")
 	workflow_url := os.Getenv("workflow_url")
 
@@ -27,7 +26,12 @@ func main() {
 		log.Fatal("event type should be specified")
 	}
 
-	message := fmt.Sprintf("{\"text\":\"*CI Failed* <%s> \ncommit <%s|%s>\nworkflow failed:%s\nworkflow_url: %s\"}", mention, commit_url, commit, workflow_name, workflow_url)
+	message := fmt.Sprintf("{\"text\":\""+
+		"*CI Failed* <%s> \n"+
+		"commit <%s|%s>\n"+
+		"workflow failed:%s\n"+
+		"workflow_url: %s\"}",
+		mention, commit_url, commit, workflow_name, workflow_url)
 	body := strings.NewReader(message)
 	_, err := http.Post(hook, "Content-type: application/json", body)
 	if err != nil {
