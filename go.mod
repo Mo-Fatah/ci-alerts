0221a09c744826c3d71043fca9d5128a526dc0eb
mohamed <mohamedabdelfatah2027@gmail.com>
2023-08-07 16:28:49 +0300

    refactor to add branch name and failed jobs

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/Mo-Fatah/ci-alerts
 
 go 1.20
 
-require github.com/actions-go/toolkit v0.0.0-20220621154146-07e42146078c // indirect
+require (
+	github.com/actions-go/toolkit v0.0.0-20220621154146-07e42146078c // indirect
+	github.com/antchfx/jsonquery v1.3.3 // indirect
+	github.com/antchfx/xpath v1.2.3 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/thedevsaddam/gojsonq/v2 v2.5.2 // indirect
+)
--- main.go
+++ main.go
@@ -3,42 +3,90 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 	"os"
 	"strings"
+
+	"github.com/antchfx/jsonquery"
 )
 
 type Context struct {
-	webhook       string
-	event         string
-	author        string
-	commit        string
-	commit_url    string
-	workflow_name string
-	workflow_url  string
+	Webhook         string
+	TriggeringEvent string
+	Branch          string
+	Author          string
+	Commit          string
+	CommitUrl       string
+	WorkflowName    string
+	WorkflowUrl     string
+	JobsUrl         string
 }
 
-func NewContext() *Context {
-	return &Context{
-		webhook:       os.Getenv("webhook"),
-		event:         os.Getenv("event"),
-		author:        os.Getenv("author"),
-		commit:        os.Getenv("commit")[0:6],
-		commit_url:    os.Getenv("commit_url"),
-		workflow_name: os.Getenv("workflow_name"),
-		workflow_url:  os.Getenv("workflow_url"),
+func NewContext(Webhook, github_context string) (*Context, error) {
+	jq, err := jsonquery.Parse(strings.NewReader(github_context))
+	if err != nil {
+		return nil, err
 	}
+
+	TriggeringEvent := jsonquery.FindOne(jq, "event/workflow_run/event").FirstChild.Data
+	Branch := jsonquery.FindOne(jq, "event/workflow_run/head_branch").FirstChild.Data
+	Author := jsonquery.FindOne(jq, "actor").FirstChild.Data
+	Commit := jsonquery.FindOne(jq, "sha").FirstChild.Data
+	repository := jsonquery.FindOne(jq, "repository").FirstChild.Data
+	CommitUrl := fmt.Sprintf("https://github.com/%s/commit/%s", repository, Commit)
+	WorkflowName := jsonquery.FindOne(jq, "event/workflow_run/name").FirstChild.Data
+	WorkflowUrl := jsonquery.FindOne(jq, "event/workflow_run/html_url").FirstChild.Data
+	JobsUrl := jsonquery.FindOne(jq, "event/workflow_run/jobs_url").FirstChild.Data
+
+	return &Context{
+		Webhook,
+		TriggeringEvent,
+		Branch,
+		Author,
+		Commit,
+		CommitUrl,
+		WorkflowName,
+		WorkflowUrl,
+		JobsUrl,
+	}, nil
 }
+
+//func getContextEnv() string {
+//
+//	file, err := os.Open("ctx")
+//	if err != nil {
+//		panic(err)
+//	}
+//	defer file.Close()
+//	var str string
+//	for {
+//		buf := make([]byte, 1024)
+//		read, _ := file.Read(buf)
+//		if read == 0 {
+//			break
+//		}
+//		str += string(buf[:read])
+//	}
+//	return str
+//}
 
 func main() {
-	fmt.Println(os.Getenv("GITHUB_CONTEXT"))
-	//context := NewContext()
-	//title := "CI Failed"
-	//if context.event == "push" {
-	//	title += " On Main"
-	//}
-	//message := buildMessage(title, context)
-	//body := strings.NewReader(message)
-	//_, err := http.Post(context.webhook, "Content-type: application/json", body)
+	webhook := os.Getenv("webhook")
+	github_context := os.Getenv("github_context")
+	context, err := NewContext(webhook, github_context)
+	if err != nil {
+		log.Fatal(err)
+	}
+	title := "CI Failed"
+	if context.TriggeringEvent == "push" {
+		title += " On Master"
+	}
+	message := buildMessage(title, context)
+	body := strings.NewReader(message)
+	fmt.Println(body)
+	//_, err = http.Post(context.Webhook, "Content-type: application/json", body)
 	//if err != nil {
 	//	log.Fatal(err)
 	//}
@@ -49,9 +97,9 @@ func buildMessage(title string, context *Context) string {
 		"type" : "section",
 		"text" : {
 			"type": "mrkdwn",
-			"text": "*%s* %s"
+			"text": "*%s* %s\nBranch: *%s*"
 		}
-	},`, title, getMention(context))
+	},`, title, getMention(context), context.Branch)
 	section := buildSection(context)
 	message := fmt.Sprintf(`{"blocks" : [ %s ], "attachments":[{ "color": "#a60021", "blocks": [ %s ] }]}`, header, section)
 	return message
@@ -59,28 +107,37 @@ func buildMessage(title string, context *Context) string {
 
 func buildSection(context *Context) string {
 	section := `{"type": "section", "fields":[`
-	commit := fmt.Sprintf(`
-		{
-			"type": "mrkdwn",
-			"text": "*Commit*\n<%s|%s>"
-		},
-	`, context.commit_url, context.commit)
-	section += commit
 
 	failed_action := fmt.Sprintf(`
 		{
 			"type": "mrkdwn",
 			"text": "*Failed Action*\n%s"
 		},
-	`, context.workflow_name)
+	`, context.WorkflowName)
 	section += failed_action
+
+	failed_job := fmt.Sprintf(`
+		{
+			"type": "mrkdwn",
+			"text": "*Failed Job*\n%s"
+		},
+	`, getFailedJob(context.JobsUrl))
+	section += failed_job
+
+	commit := fmt.Sprintf(`
+		{
+			"type": "mrkdwn",
+			"text": "*Commit*\n<%s|%s>"
+		},
+	`, context.CommitUrl, context.Commit)
+	section += commit
 
 	action_url := fmt.Sprintf(`
 		{
 			"type": "mrkdwn",
 			"text": "*Workflow Url*\n%s"
 		}
-	`, context.workflow_url)
+	`, context.WorkflowUrl)
 	section += action_url
 
 	section += `]}`
@@ -88,9 +145,9 @@ func buildSection(context *Context) string {
 }
 
 func getMention(context *Context) string {
-	if context.event == "pr" {
-		return getAuthorSlackID(context.author)
-	} else if context.event == "push" {
+	if context.TriggeringEvent == "pull_request" {
+		return getAuthorSlackID(context.Author)
+	} else if context.TriggeringEvent == "push" {
 		return "<!channel>"
 	}
 	panic("event type should be specified")
@@ -113,5 +170,34 @@ func getAuthorSlackID(author string) string {
 		}
 	}
 	os.Exit(0) // exit the program, to prevent any notification from unknown authors' PRs
+	return ""
+}
+
+func getFailedJob(url string) string {
+	res, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	bodyBytes, err := io.ReadAll(res.Body)
+
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+
+	jq, err := jsonquery.Parse(strings.NewReader(string(bodyBytes)))
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+
+	jobs := jsonquery.FindOne(jq, "jobs").ChildNodes()
+	for _, job := range jobs {
+		status := job.SelectElement("conclusion").FirstChild.Data
+		if status == "failure" {
+			return job.SelectElement("name").FirstChild.Data
+		}
+	}
 	return ""
 }
